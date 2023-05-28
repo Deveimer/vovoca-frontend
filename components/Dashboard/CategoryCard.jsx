67893be7927ad3211aa20121852cc315f08455d1
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-05-28 22:25:34 +0530

    feat: create state in CategoryCard component
     - state click to include selected class.
     - the hardcoded CategoryCards were removed.
     - were replaced with map function iterating over a list of categories and their images.

--- components/Dashboard/CategoryCard.jsx
+++ components/Dashboard/CategoryCard.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import style from "./Category.module.css";
 
-export default function CategoryCard( { musicCategory, imageURL} ) {
+export default function CategoryCard( { musicCategory, imageURL, handleClick} ) {
 
+  const [ clicked, setClicked] = useState(false);
+  
   return (
     <div className={style.category__card}>
       <div
         id="1"
-        className={style.category__card__icon}
+        className={style.category__card__icon+" "+(clicked?style.selected:'')}
         style={{
           backgroundImage: `url(${imageURL})`,
         }}
-        onClick={(e) => handleClick(e.target.id, musicCategory)}
+        onClick={(e) => {
+          setClicked(!clicked);
+          handleClick(e.target.id, musicCategory);
+        }}
       >
         <div className={style.heading}>
           <h3>{musicCategory}</h3>
