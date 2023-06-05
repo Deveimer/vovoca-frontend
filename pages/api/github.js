bd7160e0b44f0d09065872d9205d90da44159dd2
kai <kailashjat604@gmail.com>
2023-06-05 12:36:45 +0530

    refactor: declare getUsersList outside of getContributors

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,32 +1,35 @@
 const { Octokit } = require('@octokit/rest');
 
+const getUsersList = async repo => {
+  const octokit = new Octokit({
+    baseUrl: 'https://api.github.com',
+  });
+  const { data } = await octokit.rest.repos.listContributors({
+    owner: 'Deveimer',
+    repo: repo,
+  });
+  const usersList = data.map(src => {
+    return {
+      avatar_url: src.avatar_url,
+      url: src.html_url,
+    };
+  });
+  return usersList;
+};
+
 const getContributors = async (req, res) => {
   try {
-    const octokit = new Octokit({
-      baseUrl: 'https://api.github.com',
-    });
-
-    const getUsersList = async repo => {
-      const { data } = await octokit.rest.repos.listContributors({
-        owner: 'Deveimer',
-        repo: repo,
-      });
-      const usersList = data.map(src => {
-        return {
-          avatar_url: src.avatar_url,
-          url: src.html_url,
-        };
-      });
-      return usersList;
-    };
     const frontendList = await getUsersList('vovoca-frontend');
     const backendList = await getUsersList('vovoca-backend');
     const combinedList = [...frontendList, ...backendList];
     const usersList = combinedList.filter((value, index) => {
       const _value = JSON.stringify(value);
-      return index === combinedList.findIndex(obj => {
-        return JSON.stringify(obj) === _value;
-      });
+      return (
+        index ===
+        combinedList.findIndex(obj => {
+          return JSON.stringify(obj) === _value;
+        })
+      );
     });
 
     return res.status(200).json({
